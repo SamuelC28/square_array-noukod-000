c0c45250d7e8a768bfe177db78262a7d79ccefe5
Samuel Cornet <samuel@noukod.com>
2018-09-18 20:04:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,9 +3,9 @@ numbers = [1,2,3]
 def square_array(numbers)
   new_numbers = []
   numbers.each do |num|
-    num = num.square
+    num = num.square!
   return new_numbers << num
   end
 
-  numbers.collect {|num| new_numbers << num.square}
+  numbers.collect {|num| new_numbers << num.square!}
 end
