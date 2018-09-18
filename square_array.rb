9cbde9d78e508a6557e5f3aefac352f73b17d3c5
Samuel Cornet <samuel@noukod.com>
2018-09-18 19:51:06 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
 numbers = [1,2,3]
+new_numbers = []
 def square_array(numbers)
 
-numbers.each do { |num| num = num**2}
-
+  numbers.each do |num| 
+    num = num**2
+  puts new_numbers << num
+  end
 end
