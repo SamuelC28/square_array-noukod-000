cf0d222769d23bdfa832fe0672b4da12ffb0ca76
Samuel Cornet <samuel@noukod.com>
2018-09-18 20:07:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
 numbers = [1,2,3]
 
 def square_array(numbers)
-  new_numbers = []
-  numbers.each do |num|
-    num = num**2
-  puts new_numbers << num
-  end
+  # new_numbers = []
+  # numbers.each do |num|
+  #   num = num**2
+  # puts new_numbers << num
+  # end
 
-
+numbers.collect {|num| new_numbers << num**2}
 end
